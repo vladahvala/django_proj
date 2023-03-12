4025b21adb284ff2f473923e025e55ae68556eb1
vladahvala <80634246+vladahvala@users.noreply.github.com>
2023-03-12 13:19:09 +0200

    add message_counter

--- first_djando_proj-main/chat/urls.py
+++ first_djando_proj-main/chat/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path(r'', views.load_messages_home),
     path('search_user/', views.search_user, name="search_user"),
+    path('<int:pk>', views.load_messages),
     path('ajax/<int:pk>', views.load_msgAJAX, name="chatroom-ajax") 
 ]
--- first_djando_proj-main/chat/views.py
+++ first_djando_proj-main/chat/views.py
@@ -4,8 +4,14 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
 from django.http.response import JsonResponse
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from .models import Message
 import json
+
+def unread_msgs(request, sender):
+    msgs = Message.objects.filter(Q(seen=False) & Q(receiver=request.user, sender=sender))
+    return msgs.count()
+
 
 @login_required
 def load_messages_home(request): #запит на чат із певною людиною
@@ -22,14 +28,22 @@ def load_messages(request, pk, users=None):
     other_user = get_object_or_404(User, pk=pk)
     messages = Message.objects.filter( Q(sender=request.user), Q(receiver=other_user))
     messages.update(seen=True)
+    messages = messages | Message.objects.filter( Q(receiver=other_user), Q(sender=request.user))
 
     if not users: #якщо юзера не знаходить, то показуємо всіх користувачів
         users = User.objects.all() 
+
+    print("Словник")
+
+    unread_num_dict = {} #{"other_user"}: 19, ...
+    for usr in users:
+        unread_num_dict[usr.username] = unread_msgs(request, usr)
 
     context = {
         "other_user": other_user,
         "messages": messages,
         "users": users,
+        "unread_num_dict": unread_num_dict
     }
 
     return render(request, "privatChat.html", context)
@@ -44,7 +58,7 @@ def load_msgAJAX(request, pk):
         message_list.append({
             "sender": msg.sender.username,
             "message": msg.text,
-            "date_created": msg.date_created
+            "date_created": naturaltime(msg.date_created)
         })
         msg.seen = True
     messages.update(seen=True)
@@ -57,7 +71,7 @@ def load_msgAJAX(request, pk):
             message_list.append({
             "sender": m.sender.username,
             "message": m.text,
-            "date_created": m.date_created
+            "date_created": naturaltime(m.date_created)
             })
     return JsonResponse(message_list, safe=False)
 
